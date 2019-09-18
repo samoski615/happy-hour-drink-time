b0718304fd54d4a1c138694bffaef86849b527b3
Josi <JosiGrc@users.noreply.github.com>
2019-09-18 08:34:54 -0500

    added more properties to the drink specials table and also deleted some repeating info from the Bar table and deleted the deleted info from the bar views

--- Models/DrinkSpecial.cs
+++ Models/DrinkSpecial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,10 +12,28 @@ namespace HappyHourTracker.Models
         [Key]
         public int Id { get; set; }
 
-        public string drink { get; set; }
+        [Display(Name = "Day that the drink special occurs:")]
+        public string DayOfTheWeek { get; set; }
+
+        [Display(Name = "Drink")]
+        public string TypeOfdrink { get; set; }
+
+        [Display(Name = "Main alcohol in drink")]
+        public string MainTypeOfAlcohol { get; set; }
+
+        [Display(Name = "Price for HappyHour special")]
+        public double DrinkPrice { get; set; }
+
+        [Display(Name = "Time that HappyHour starts")]
+        public string TimeHappyHourStarts { get; set; }
+
+        [Display(Name = "Time that HappyHour ends")]
+        public string TimeHappyHourEnds { get; set; }
 
         [ForeignKey("ApplicationUser")]
         public string ApplicationId { get; set; }
-        public ApplicationUser ApplicationUser { get; set; }
+
+        public enum DayOfWeek { }
+
     }
 }
