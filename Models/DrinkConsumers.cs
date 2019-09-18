e8ba0acf52def3f79b8131fb5971c6c71356cd14
samoski615 <samoski615@gmail.com>
2019-09-18 09:24:25 -0500

    committing changes before pulling

--- Models/DrinkConsumers.cs
+++ Models/DrinkConsumers.cs
@@ -12,27 +12,22 @@ namespace HappyHourTracker.Models
         public int Id { get; set; }
 
         [Display(Name = "First Name")]
-        public string firstName { get; set; }
+        public string FirstName { get; set; }
 
         [Display(Name = "Last Name")]
-        public string lastName { get; set; }
+        public string LastName { get; set; }
 
         [Display(Name = "Address")]
-        public string address { get; set; }
+        public string Address { get; set; }
 
         [Display(Name = "City")]
-        public string city { get; set; }
+        public string City { get; set; }
 
         [Display(Name = "State")]
-        public string state { get; set; }
+        public string State { get; set; }
 
         [Display(Name = "Zipcode")]
-        public int zipcode { get; set; }
-
-        [Display(Name = "Favorite type of Bar")]
-        public string favoriteBarType { get; set; }
-
-
+        public string Zipcode { get; set; }
 
     }
 }
